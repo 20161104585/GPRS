19ebef0aeebda8df89d030a6b08ae96b8df92bb5
20161104585 <1663512557@qq.com>
2017-07-02 20:53:49 +0800

    成功
    /Users/a20161104585/Desktop/GPRS/GPRS/main.c

--- GPRS/main.c
+++ GPRS/main.c
@@ -10,14 +10,20 @@
 int main()
 {
     FILE *fp=fopen("//Users//a20161104585//Desktop//GPRS//GPRS//GPS170510.log", "r");
-    FILE *fp1=fopen("//Users//a20161104585//Desktop//GPRS/GPRS//output.txt", "r+");
-    int i,a,b,c;
+    FILE *fp1=fopen("//Users//a20161104585//Desktop//GPRS/GPRS//output.csv", "r+");
+    int i,a,b,c,d;
+    int h1,h2,m1,m2,s1,s2,Y1,Y2,M1,M2,D1,D2;
     char g1[63];
     char g2[70];
-    char lat[8];//纬度
-    char lng[9];//经度
-    char time[6];//时间
-    char alt[4];//海拔
+    char lat[9];//纬度
+    char lat1[3];
+    char lat2[6];
+    char lng[10];//经度
+    char lng1[4];
+    char lng2[6];
+    char day[7];//日期
+    char time[7];//时间
+    char alt[5];//海拔
     if(fp==NULL)
     {
         printf("文件读取错误，请检查文件GPS170510.log是否存在!");
@@ -32,29 +38,76 @@ int main()
                 lat[i]=g1[i+16];
                 lat[8]='\0';
             }
-            printf("纬度：%s\n",lat);
-            fprintf(fp1,"纬度：%s\t",lat);
+            lat1[0]=lat[0];
+            lat1[1]=lat[1];
+            lat2[0]=lat[2];
+            lat2[1]=lat[3];
+            lat2[2]=lat[5];
+            lat2[3]=lat[6];
+            lat2[4]=lat[7];
+            printf("纬度：%s‘%s\n",lat1,lat2);
+            fprintf(fp1,"纬度：%s‘%s,",lat1,lat2);
+            
+            
             for(a=0;a<9;a++)
             {
                 lng[a]=g1[a+27];
                 lng[9]='\0';
             }
-            printf("经度：%s\n",lng);
-            fprintf(fp1,"经度：%s\t",lng);
+            lng1[0]=lng[0];
+            lng1[1]=lng[1];
+            lng1[2]=lng[2];
+            lng2[0]=lng[3];
+            lng2[1]=lng[4];
+            lng2[2]=lng[6];
+            lng2[3]=lng[7];
+            lng2[4]=lng[8];
+            printf("经度：%s‘%s\n",lng1,lng2);
+            fprintf(fp1,"经度：%s’%s,",lng1,lng2);
+            
+            
             for(b=0;b<6;b++)
             {
-                time[b]=g1[b+51];
-                time[6]='\0';
+                day[b]=g1[b+51];
+                day[6]='\0';
             }
-            printf("时间：%s\n",time);
-            fprintf(fp1,"时间：%s\t",time);
+            Y1=day[4]-'0';
+            Y2=day[5]-'0';
+            M1=day[2]-'0';
+            M2=day[3]-'0';
+            D1=day[0]-'0';
+            D2=day[1]-'0';
+            printf("日期：%d%d年%d%d月%d%d日\n",Y1,Y2,M1,M2,D1,D2);
+            fprintf(fp1,"日期：%d%d年%d%d月%d%d日,",Y1,Y2,M1,M2,D1,D2);
+            
+            
+            for(d=0;d<2;d++)
+            {
+                time[d]=g1[d+7];
+                time[2]='\0';
+            }
+            h1=(time[0]-'0')*10;
+            h2=(time[1]-'0')*1+h1+8;
+            for(d=0;d<4;d++)
+            {
+                time[d]=g1[d+9];
+                time[4]='\0';
+            }
+            m1=time[0]-'0';
+            m2=time[1]-'0';
+            s1=time[2]-'0';
+            s2=time[3]-'0';
+            printf("时间：%d时%d%d分%d%d秒\n",h2,m1,m2,s1,s2);
+            fprintf(fp1,"时间：%d时%d%d分%d%d秒,",h2,m1,m2,s1,s2);
+            
+            
             for(c=0;c<4;c++)
             {
                 alt[c]=g2[c+43];
                 alt[4]='\0';
             }
-            printf("海拔：%s\n",alt);
-            fprintf(fp1,"海拔：%s\n",alt);
+            printf("海拔：%sm\n",alt);
+            fprintf(fp1,"海拔：%sm\n",alt);
         }
     }
     fclose(fp);
